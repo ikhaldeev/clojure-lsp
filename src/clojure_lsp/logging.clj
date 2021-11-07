f7d5f625a8c3e175af4723d80a2a2bfbe3fe282a
Ivan Khaldeev <ikhaldeev@yandex.ru>
2021-11-08 00:57:01 +0400

    Use custom output-fn for logger. Remove hostname from output.

--- src/clojure_lsp/logging.clj
+++ src/clojure_lsp/logging.clj
@@ -1,13 +1,28 @@
 (ns clojure-lsp.logging
   (:require
-   [taoensso.timbre :as log]))
+    [clojure.string :as str]
+    [taoensso.timbre :as log]
+    [taoensso.encore :as enc]))
 
 (set! *warn-on-reflection* true)
+
+(defn output-fn
+  ([data]
+   (let [{:keys [level ?err #_vargs msg_ ?ns-str ?file
+                 timestamp_ ?line]} data]
+     (str
+       (when-let [ts (force timestamp_)] (str ts " "))
+       (str/upper-case (name level))  " "
+       "[" (or ?ns-str ?file "?") ":" (or ?line "?") "] - "
+       (force msg_)
+       (when-let [err ?err]
+         (str enc/system-newline (log/stacktrace err {})))))))
 
 #_{:clj-kondo/ignore [:unresolved-var]}
 (defn setup-logging [db]
   (let [log-path (str (java.io.File/createTempFile "clojure-lsp." ".out"))]
-    (log/merge-config! {:appenders {:println {:enabled? false}
+    (log/merge-config! {:output-fn output-fn
+                        :appenders {:println {:enabled? false}
                                     :spit (log/spit-appender {:fname log-path})}})
     (log/handle-uncaught-jvm-exceptions!)
     (swap! db assoc :log-path log-path)))
